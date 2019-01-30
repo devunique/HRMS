0596a077f6710fe5f65bc645c713f1afc57c027a
sarav <saravanan@uniquesystem.co.in>
2019-01-30 16:19:07 +0530

    First commit

---
+++ hrms/src/main/java/com/unique/hrms/HrmsApplication.java
@@ -0,0 +1,14 @@
+package com.unique.hrms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmsApplication.class, args);
+	}
+
+}
+
